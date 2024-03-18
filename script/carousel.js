271d440cce36636f863ebb04b80069ec7fa00f56
Valentin Guillot <97942568+mrvalbass@users.noreply.github.com>
2024-03-18 23:32:01 +0100

    Add french version, journey data and style updates

--- script/carousel.js
+++ script/carousel.js
@@ -8,18 +8,34 @@ const dates = Array.from(navDates.children);
 const frenchMap = document.querySelector('#map')
 const cities = Array.from(frenchMap.children).slice(1)
 
-const cardWidth= cards[0].getBoundingClientRect().width;
+const getCardsWidth = () => cards[0].getBoundingClientRect().width;
+// Function and not static expression to be dynamically calculated each time called
+// Otherwise when resizing the carousel, cards would keep the initial loading size
 
 //Place cards side by side
 const setCardPosition = (card, index) => {
-  card.style.left = `${index*cardWidth}px`
+  card.style.left = `${index*getCardsWidth()}px`
 };
 
-cards.forEach(setCardPosition);
-
-//Resize cards on carousel size modification
-const resizeObserver = new ResizeObserver(() =>cards.forEach(setCardPosition))
-resizeObserver.observe(journeyList)
+// Remove arrow at the beginning and at the end of the carousel
+const displayArrows = (targetIndex) => {
+  if (targetIndex === cards.length -1) {
+      rightArrow.style.visibility = 'hidden'
+      rightArrow.style.opacity = 0
+      leftArrow.style.visibility = 'visible'
+      leftArrow.style.opacity = 1
+    } else if (targetIndex === 0) {
+      rightArrow.style.visibility = 'visible'
+      rightArrow.style.opacity = 1
+      leftArrow.style.visibility = 'hidden'
+      leftArrow.style.opacity = 0
+    } else {
+      rightArrow.style.visibility = 'visible'
+      rightArrow.style.opacity = 1
+      leftArrow.style.visibility = 'visible'
+      leftArrow.style.opacity = 1
+    }
+}
 
 // Move to a target card
 const moveToCard = (targetIndex) => {
@@ -41,16 +57,7 @@ const moveToCard = (targetIndex) => {
     currentCity.classList.remove('current-city')
     cities[targetIndex].classList.add('current-city')
 
-    if (targetIndex === cards.length -1) {
-      rightArrow.style.visibility = 'hidden'
-      leftArrow.style.visibility = 'visible'
-    } else if (targetIndex === 0) {
-      rightArrow.style.visibility = 'visible'
-      leftArrow.style.visibility = 'hidden'
-    } else {
-      rightArrow.style.visibility = 'visible'
-      leftArrow.style.visibility = 'visible'
-    }
+    displayArrows(targetIndex)
   }
 }
 
@@ -68,7 +75,7 @@ carousel.addEventListener('click', e => {
   }
 })
 
-// Move to a card associated with a date
+// Move to a card with  dates
 navDates.addEventListener('click', e => {
   const targetDate = e.target.closest('button');
   if (!targetDate) return
@@ -76,7 +83,7 @@ navDates.addEventListener('click', e => {
   moveToCard(targetIndex);
 })
 
-// Move to a card associated with a place 
+// Move to a card with places 
 frenchMap.addEventListener('click', e => {
   const targetCity = e.target.closest('ellipse');
   if (!targetCity) return;
@@ -87,6 +94,16 @@ frenchMap.addEventListener('click', e => {
     carousel.scrollIntoView()
   };
 })
+//Resize cards on carousel size modification (gets triggered on page load too)
+const carouselResizeObserver = new ResizeObserver(() => {
+  cards.forEach(setCardPosition)
+  // Move to current card to avoid displaying the gap between 2 cards
+  const currentIndex = cards.indexOf(journeyList.querySelector('.current-card'))
+  journeyList.style.transition = 'none'
+  moveToCard(currentIndex)
+  setTimeout(() => journeyList.style.transition = null, 1)
+})
 
+carouselResizeObserver.observe(carousel)
 // Open the page on the last element
 moveToCard(cards.length-1)
